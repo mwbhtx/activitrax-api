301e72a3c24d2c3463a3d030ebd84ec29fe77eb3
Matthew <mwbhtx@outlook.com>
2023-01-10 02:25:28 -0700

    fixed circuluar bullshit

--- src/auth0/auth0.router.js
+++ src/auth0/auth0.router.js
@@ -1,22 +1,17 @@
 const express = require("express");
 
-const axios = require("axios");
-
 const { validateAccessToken } = require("../middleware/auth0.middleware.js");
 
 const auth0Router = express.Router();
 
 const _ = require('lodash');
 
-const {
-    searchAuth0UserByStravaId
-} = require("../auth0/auth0.service");
-const { getUserConfigForClient, deleteUserConnectionData } = require("../mongo/mongoservice.js");
+const mongo = require("../mongo/mongoservice.js");
 
 auth0Router.get("/user_config", validateAccessToken, async (req, res) => {
     try {
         const user_id = req.auth.payload.sub;
-        const config = await getUserConfigForClient(user_id);
+        const config = await mongo.getUserConfigForClient(user_id);
         res.status(200).json(config);
     } catch (error) {
         const error_message = _.get(error, 'response.data');
@@ -25,27 +20,12 @@ auth0Router.get("/user_config", validateAccessToken, async (req, res) => {
     }
 })
 
-auth0Router.get('/user/:strava_id', validateAccessToken, async (req, res) => {
-
-    try {
-        const strava_id = req.params.strava_id;
-        const user = await searchAuth0UserByStravaId(strava_id);
-        res.status(200).json(user);
-    }
-    catch (error) {
-        const error_message = _.get(error, 'response.data');
-        console.log(JSON.stringify(error_message) || error);
-        res.status(500).json({ message: 'server error' });
-    }
-
-})
-
 auth0Router.post('/disconnect_service', validateAccessToken, async (req, res) => {
 
     try {
         const uid = req.auth.payload.sub;
         const service = req.body.service_name;
-        await deleteUserConnectionData(uid, service);
+        await mongo.deleteUserConnectionData(uid, service);
         res.status(200).json({ message: 'success' });
     }
     catch (error) {
